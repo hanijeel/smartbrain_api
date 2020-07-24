f07d408f4af626d0b24844083641c386e3218165
hanijeel <leejinah7368@gmail.com>
2020-07-24 14:47:19 -0400

    initial commit

---
+++ controllers/profile.js
@@ -0,0 +1,16 @@
+const handleProfileGet = (req, res, db) =>{
+	const { id } = req.params;
+	db.select('*').from('users').where({ id	})
+	.then(user => {
+		if(user.length) {
+			res.json(user[0]);
+		} else {
+			res.status(400).json('Profile not found')
+		}
+	})
+	.catch(err => res.status(400).json('Profile error getting user'))
+}
+
+module.exports = {
+	handleProfileGet
+}
